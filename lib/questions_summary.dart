5941fb2d76cb79f8d94bc4baf447a607ad058ac4
Oziel Martínez <omartinez@syscap.com.mx>
2024-10-06 01:06:35 -0600

    feat: Add scroll view to summary

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -7,23 +7,28 @@ class QuestionsSummary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: summaryData.map((item) {
-        return Row(
-          children: [
-            Text(((item['question_index'] as int) + 1).toString()),
-            Expanded(
-              child: Column(children: [
-                Text(item['question'] as String),
-                const SizedBox(height: 10,),
-                Text(item['user_answer'] as String),
-                Text(item['correct_answer'] as String),
-                        
-              ],),
-            )
-          ],
-        );
-      }).toList(),
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map((item) {
+            return Row(
+              children: [
+                Text(((item['question_index'] as int) + 1).toString()),
+                Expanded(
+                  child: Column(children: [
+                    Text(item['question'] as String),
+                    const SizedBox(height: 10,),
+                    Text(item['user_answer'] as String),
+                    Text(item['correct_answer'] as String),
+                            
+                  ],),
+                )
+              ],
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
